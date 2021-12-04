c06395873019a30aebe9c193fc08c132b5509780
Roger Guldbrandsen <8797880+kinbiko@users.noreply.github.com>
2021-12-04 20:32:25 +0900

    fix: Trim whitespace before returning (#7)

--- cmd/kokodoko/main.go
+++ cmd/kokodoko/main.go
@@ -17,7 +17,7 @@ import (
 
 // Globals are used to enable Bugsnag.
 // Must be updated per release.
-const appVersion = "0.2.1"
+const appVersion = "0.2.2"
 
 //nolint:gochecknoglobals // The following are intended to be set via ldflags, for developers.
 var (
